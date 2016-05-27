4bee9cd2ff38dc35ec0bf003b4f3ce12dfc8a2b0
Nguyen Quy Hy <nguyenquyhy@live.com.sg>
2016-05-27 16:44:45 +0800

    Initial setup

---
+++ src/main/java/com/nguyenquyhy/spongesubwhitelister/SpongeSubWhitelister.java
@@ -0,0 +1,11 @@
+package com.nguyenquyhy.spongesubwhitelister;
+
+import org.spongepowered.api.plugin.Plugin;
+
+/**
+ * Created by Hy on 5/27/2016.
+ */
+@Plugin(id = "com.nguyenquyhy.spongesubwhitelister", name="Sponge SubWhitelister")
+public class SpongeSubWhitelister {
+
+}
